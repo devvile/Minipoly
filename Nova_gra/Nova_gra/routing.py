6b217df2d5bde4457118504fb5b0d7020231a225
Dottore <patryk.czemierowski@gmail.com>
2020-11-03 12:38:37 +0100

    [WS] game routing.py created, Nova_gra routing.py configured

--- Nova_gra/Nova_gra/routing.py
+++ Nova_gra/Nova_gra/routing.py
@@ -1,4 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import game.routing
 
 application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            game.routing.websocket_urlpatterns
+        )
+    ),
 })
