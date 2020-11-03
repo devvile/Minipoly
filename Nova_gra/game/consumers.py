5a6f421668c16a2844e2a106e365500c172f6ee6
Dottore <patryk.czemierowski@gmail.com>
2020-11-03 22:03:44 +0100

    [Channels] get_player, get_game_is_played, get_player_in_game getters, REady button conditions

--- Nova_gra/game/consumers.py
+++ Nova_gra/game/consumers.py
@@ -10,6 +10,7 @@ class GameEventsConsumer(AsyncWebsocketConsumer):
         self.game = await database_sync_to_async(self.get_game)(id=game_id)
         self.room_group_name = self.game.name
 
+
         await (self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
@@ -20,17 +21,39 @@ class GameEventsConsumer(AsyncWebsocketConsumer):
     def get_game(self,id):
         return Game.objects.get(pk=id)
 
+
+    def get_player(self, usr):
+        return Player.objects.get(nick=usr)
+
+
+    def get_game_is_played(self, game):
+        return game.is_played
+
+
+    def get_player_in_game(self,player):
+        return player.in_game
+
+
     async def receive(self, text_data):
+
         """
         if text_data == "klik":
             self.counter.klik +=1
         elif text_data == "klak":
             self.counter.key += 1
             """
+        game = self.game
         message = json.loads(text_data)
-        if message['action']=="ready":
-            print("Bingo!")
-        elif message['action']=="start":
+        action = message['action']
+        game.player = await database_sync_to_async(self.get_player)(message['player'])
+        player = game.player
+        game.is_played = await database_sync_to_async(self.get_game_is_played)(game)
+        player.in_game = await database_sync_to_async(self.get_player_in_game)(player)
+
+        if action=="ready":
+            if not game.is_played and not player.in_game:  #and  player not in game.players_ready
+                print("KRUCZAK")
+        elif action=="start":
             print("Starto!")
         await database_sync_to_async(self.game.save)()
         await (self.channel_layer.group_send)(
