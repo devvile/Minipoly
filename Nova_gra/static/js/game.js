6122ba6ab878e2feca4c00f4ccea1df79f5fd0b2
Dottore <patryk.czemierowski@gmail.com>
2020-11-03 13:35:31 +0100

    [WS] Websocket echo server for buttons

--- Nova_gra/static/js/game.js
+++ Nova_gra/static/js/game.js
@@ -1,20 +1,46 @@
 function main() {
-  configWebsocket();
-  asignEvents();
+  configGame();
 }
 
-function configWebsocket() {
+function configGame() {
   const socket = "ws://" + window.location.host + "/";
   websocket = new WebSocket(socket);
+
+  function asignEvents() {
+    const ready_btn = document.querySelector(".--ready_btn");
+    const start_btn = document.querySelector(".--start_btn");
+    ready_btn.addEventListener("click", () => {
+      sendMess("Ready");
+    });
+    start_btn.addEventListener("click", () => {
+      sendMess("Start");
+    });
+  }
 
   function openWebsocket() {
     console.log("Establishing Websocket Connection...");
     websocket.onopen = () => {
-      console.log("Connection Established!");
+      console.log("Websocket Connection Established!");
       console.log(websocket);
     };
-    openWebsocket();
   }
+  function setWebsocket() {
+    websocket.onmessage = (mess) => {
+      dataJson = JSON.parse(mess.data);
+      console.log(mess.data);
+      console.log(`Message:  ${mess.data}`);
+    };
+
+    websocket.onclose = () => {
+      console.log("Websocket Connection Terminated!");
+    };
+  }
+  function sendMess(messText) {
+    websocket.send(messText);
+  }
+  asignEvents();
+  openWebsocket();
+  setWebsocket();
 }
 
 // Asigning Event Listneres to DOM ELEMENTS
