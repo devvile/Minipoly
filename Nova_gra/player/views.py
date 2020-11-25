26f4addda64551d1b3f6dd7ad8466b5de0f4dcbe
Dottore <patryk.czemierowski@gmail.com>
2020-11-25 13:40:03 +0100

    [REFACTOR] PEP 8 corrections in player, Notifications deleted in code

--- Nova_gra/player/views.py
+++ Nova_gra/player/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from game.models import Game, Notification
+from game.models import Game
 from .models import Player
 from .forms import ChangeNick, ChangeDescription
 from game.forms import CreateGame
@@ -11,19 +11,18 @@ def home(request):
     player = Player.objects.get(parent=usr)
     games = Game.objects.all()
     form = CreateGame
-    args = {'usr': usr, 'games': games, 'form' : form,'player':player}
-    return render(request,'player/home.html', args)
+    args = {'usr': usr, 'games': games, 'form': form, 'player':player}
+    return render(request, 'player/home.html', args)
 
 @login_required
 def account(request, acc, notif):
-    usr =request.user
+    usr = request.user
     player = Player.objects.get(parent=usr.username)
     form = ChangeNick
     hs = Game.objects.filter(host=player.nick)
     hs_nr = hs.count()
-    noti = Notification.objects.get(name=notif)
     desc = player.description
-    args = {'player':player,'form':form, 'games':hs, 'games_nr':hs_nr,'notification':noti.note, 'description':desc}
+    args = {'player': player, 'form': form, 'games': hs , 'games_nr': hs_nr, 'description': desc}
     return render(request, 'player/player_account.html', args)
 
 @login_required
@@ -35,15 +34,14 @@ def new_nick(request):
     if hs_nr == 0:
         if request.method == "POST":
             acc = player
-            form = ChangeNick(instance = acc,data=request.POST)
+            form = ChangeNick(instance=acc, data=request.POST)
             if form.is_valid():
                 form.save()
-                return redirect("player_account",player, 'nick_success')
+                return redirect("player_account", player, 'nick_success')
         else:
-            return redirect("player_account",player, 'nick_not_valid')
+            return redirect("player_account", player, 'nick_not_valid')
     else:
-        noti = Notification.objects.get(name='nickchange_forbidden')
-        return redirect("player_account", player, noti.name)
+        return redirect("player_account", player)
 
 @login_required
 def edit_description(request, acc):
@@ -52,4 +50,4 @@ def edit_description(request, acc):
     player = Player.objects.get(parent=usr.username)
     desc = player.description
     args = {"form":form, "description":desc}
-    return render (request, 'player/player_edit_description.html',args)
+    return render (request, 'player/player_edit_description.html', args)
